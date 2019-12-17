0d72b819cc92d384bc71d2a285853761334e7ce6
Guillaume Berche <guillaume.berche@orange.com>
2019-12-17 02:32:24 +0100

    Only run OsbCmdbApplicationTests when system properties are properly defined. Exclude them in circle

--- osb-cmdb/src/test/java/com/orange/oss/osbcmdb/OsbCmdbApplicationTests.java
+++ osb-cmdb/src/test/java/com/orange/oss/osbcmdb/OsbCmdbApplicationTests.java
@@ -1,10 +1,16 @@
 package com.orange.oss.osbcmdb;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Only run OsbCmdbApplicationTests when system properties are properly defined. Exclude them in circle
+ */
 @SuppressWarnings("WeakerAccess")
 @SpringBootTest
+@EnabledIfEnvironmentVariable(named = "acceptanceTests", matches = "true")
 public class OsbCmdbApplicationTests {
 
     @Test
