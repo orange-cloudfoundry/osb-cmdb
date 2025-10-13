4caad725c4380093cf6368ba98aa027f2f08ad78
Guillaume Berche <guillaume.berche@orange.com>
2025-10-13 18:27:37 +0200

    Implement Async Binding Support
    Intermediate commit, compile and unit tests OK
    
    - getLastOperation
    - get service binding

--- osb-cmdb/src/main/java/com/orange/oss/osbcmdb/servicebinding/ServiceBindingInterceptor.java
+++ osb-cmdb/src/main/java/com/orange/oss/osbcmdb/servicebinding/ServiceBindingInterceptor.java
@@ -6,6 +6,10 @@ import org.springframework.cloud.servicebroker.model.binding.CreateServiceInstan
 import org.springframework.cloud.servicebroker.model.binding.CreateServiceInstanceBindingResponse;
 import org.springframework.cloud.servicebroker.model.binding.DeleteServiceInstanceBindingRequest;
 import org.springframework.cloud.servicebroker.model.binding.DeleteServiceInstanceBindingResponse;
+import org.springframework.cloud.servicebroker.model.binding.GetLastServiceBindingOperationRequest;
+import org.springframework.cloud.servicebroker.model.binding.GetLastServiceBindingOperationResponse;
+import org.springframework.cloud.servicebroker.model.binding.GetServiceInstanceBindingRequest;
+import org.springframework.cloud.servicebroker.model.binding.GetServiceInstanceBindingResponse;
 
 /**
  * Supports intercepting OSB service provisionning calls, mainly for acceptance test purposes. Reuses prototypes from
@@ -15,10 +19,18 @@ public interface ServiceBindingInterceptor {
 
 	boolean accept(CreateServiceInstanceBindingRequest request);
 
+	boolean accept(GetLastServiceBindingOperationRequest request);
+
+	boolean accept(GetServiceInstanceBindingRequest request);
+
 	boolean accept(DeleteServiceInstanceBindingRequest request);
 
 	Mono<CreateServiceInstanceBindingResponse> createServiceInstanceBinding(
 		CreateServiceInstanceBindingRequest request);
+
+	Mono<GetLastServiceBindingOperationResponse> getLastOperation(GetLastServiceBindingOperationRequest request);
+
+	Mono<GetServiceInstanceBindingResponse> getServiceInstanceBinding(GetServiceInstanceBindingRequest request);
 
 	Mono<DeleteServiceInstanceBindingResponse> deleteServiceInstanceBinding(
 		DeleteServiceInstanceBindingRequest request);
