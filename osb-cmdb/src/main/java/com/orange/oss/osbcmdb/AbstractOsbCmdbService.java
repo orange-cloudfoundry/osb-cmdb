2ecd43144aa44029bbe7395ca52ce9fa38b91085
Guillaume Berche <guillaume.berche@orange.com>
2025-11-12 19:15:30 +0100

    Revert "Failed attempt to convert lookupOrCreateSpace() to V3. Should rather try to contribute it to cf-java-client"
    This reverts commit 2a2505cc78830ab7edd16ad000b281581b4ff279.

--- osb-cmdb/src/main/java/com/orange/oss/osbcmdb/AbstractOsbCmdbService.java
+++ osb-cmdb/src/main/java/com/orange/oss/osbcmdb/AbstractOsbCmdbService.java
@@ -2,10 +2,7 @@ package com.orange.oss.osbcmdb;
 
 import org.cloudfoundry.client.CloudFoundryClient;
 import org.cloudfoundry.client.v2.organizations.ListOrganizationSpacesRequest;
-import org.cloudfoundry.client.v3.Relationship;
-import org.cloudfoundry.client.v3.ToOneRelationship;
-import org.cloudfoundry.client.v3.spaces.CreateSpaceRequest;
-import org.cloudfoundry.client.v3.spaces.SpaceRelationships;
+import org.cloudfoundry.client.v2.spaces.CreateSpaceRequest;
 import org.cloudfoundry.operations.CloudFoundryOperations;
 import org.cloudfoundry.operations.DefaultCloudFoundryOperations;
 import org.cloudfoundry.operations.organizations.OrganizationDetail;
@@ -101,14 +98,8 @@ public class AbstractOsbCmdbService {
 		return getSpaceId(spaceName)
 			.switchIfEmpty(Mono.just(this.defaultOrg)
 				.flatMap(orgName -> getOrganizationId(orgName)
-					.flatMap(orgId -> client.spacesV3().create(CreateSpaceRequest.builder()
-						.relationships(SpaceRelationships.builder()
-							.organization(ToOneRelationship.builder()
-								.data(Relationship.builder()
-									.
-									.build())
-								.build())
-							.build())organizationId(orgId)
+					.flatMap(orgId -> client.spaces().create(CreateSpaceRequest.builder()
+						.organizationId(orgId)
 						.name(spaceName)
 						.build())
 						.doOnSuccess(response -> LOG.info("Created space {}", spaceName))
